980e756a65adbc3471d7e7cc3e977ae9cd9bca5c
austinoso <contact@austinosor.io>
2020-11-15 23:10:46 -0800

    firebase setup

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const LANDING = "/";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
+export const HOME = "/home";
+export const USER = "/user";
+export const CHAT = "/chat";
+export const PASSWORD_FORGET = "/pw-forget";
